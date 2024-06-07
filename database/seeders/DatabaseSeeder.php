57ebc02dc3da9d2025d30254a51407d5cd9e593f
datboimech <47047103+datboimech@users.noreply.github.com>
2024-06-06 19:51:41 -0500

    Updating DatabaseSeeder to generate less Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -109,11 +109,11 @@ class DatabaseSeeder extends Seeder
         $this->command->info('Blog categories created.');
 
         $this->command->warn(PHP_EOL . 'Creating blog authors and posts...');
-        $this->withProgressBar(20, fn () => Author::factory(1)
+        $this->withProgressBar(5, fn () => Author::factory(1)
             ->has(
-                Post::factory()->count(5)
+                Post::factory()->count(1)
                     ->has(
-                        Comment::factory()->count(rand(5, 10))
+                        Comment::factory()->count(rand(1, 2))
                             ->state(fn (array $attributes, Post $post) => ['customer_id' => $customers->random(1)->first()->id]),
                     )
                     ->state(fn (array $attributes, Author $author) => ['blog_category_id' => $blogCategories->random(1)->first()->id]),
